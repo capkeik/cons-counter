bdd3f5fee8a1b17b236b045027186f194ec2bc98
Azat Khamzin <AzRKhamzin@gmail.com>
2023-06-06 04:39:17 +0300

    add auth DSL

--- src/main/scala/services/Auth.scala
+++ src/main/scala/services/Auth.scala
@@ -6,7 +6,7 @@ import pdi.jwt.JwtClaim
 trait AuthAlg[F[_]] {
   def newUser(userName: UserName, password: Password): F[JwtToken]
   def login(userName: UserName, password: Password): F[JwtToken]
-  def ogout(token: JwtToken, userName: UserName): F[Unit]
+  def logout(token: JwtToken, userName: UserName): F[Unit]
 }
 
 trait UserAuth[F[_], A] {
---
+++ src/main/scala/services/Users.scala
@@ -0,0 +1,8 @@
+package services
+
+import domain.auth.{EncryptedPassword, User, UserId, UserName}
+
+trait Users[F[_]] {
+  def find(username: UserName): F[Option[User]]
+  def create(username: UserName, password: EncryptedPassword): F[UserId]
+}

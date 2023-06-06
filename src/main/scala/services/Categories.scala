632419655ef9e1ea4d23f528ae3ca4ac5c33c60a
Azat Khamzin <AzRKhamzin@gmail.com>
2023-06-06 05:04:12 +0300

    stop to implement jwt

--- src/main/scala/services/Categories.scala
+++ src/main/scala/services/Categories.scala
@@ -1,7 +1,10 @@
 package services
 
+import cats.effect.Resource
+import cats.effect.kernel.MonadCancelThrow
 import domain.auth.UserId
 import domain.category.{Category, CategoryId, CategoryName, CreateCategory}
+import doobie.util.transactor.Transactor
 
 trait Categories[F[_]] {
   def findAll(userId: UserId): F[List[Category]]
@@ -9,4 +12,22 @@ trait Categories[F[_]] {
   def findOutcomes(userId: UserId): F[List[Category]]
   def create(userId: UserId, createCategory: CreateCategory): F[CategoryId]
   def delete(userId: UserId, categoryId: CategoryId): F[Unit]
+}
+
+object Categories {
+  def make[F[_]: MonadCancelThrow](
+    transactor: Transactor[F]
+  ): Categories[F] = {
+    new Categories[F] {
+      override def findAll(userId: UserId): F[List[Category]] = ???
+
+      override def findIncomes(userId: UserId): F[List[Category]] = ???
+
+      override def findOutcomes(userId: UserId): F[List[Category]] = ???
+
+      override def create(userId: UserId, createCategory: CreateCategory): F[CategoryId] = ???
+
+      override def delete(userId: UserId, categoryId: CategoryId): F[Unit] = ???
+    }
+  }
 }
